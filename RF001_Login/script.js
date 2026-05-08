3e49809cd6a10a5a54a9bf8c52cd1694a2a6b99c
brenuzy <victor.andrade.costa@escola.pr.gov.br>
2026-05-08 08:23:42 -0300

    test

--- RF001_Login/script.js
+++ RF001_Login/script.js
@@ -2,3 +2,15 @@ function showForm (formId) {
     document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"));
     document.getElementById(formId).classList.add("active");
 }   
+document.querySelector("button[type='submit']").addEventListener("click", function(e) {
+  e.preventDefault();
+
+  const email = document.querySelector("input[type='email']").value;
+  const senha = document.querySelector("input[type='password']").value;
+
+  if (email === "admin@email.com" && senha === "1234") {
+    window.location.href = "../RF002_Pg_Inicial/index.html";
+  } else {
+    document.getElementById("mensagem-erro").style.display = "block";
+  }
+});
